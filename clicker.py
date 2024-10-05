0d453c1bfb30fc175380010b1450f4753188d1c7
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-10-05 20:55:22 +0530

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -13,7 +13,30 @@ import time
 import json
 from threading import Thread, active_count
 from concurrent.futures import ThreadPoolExecutor
+from telethon.extensions import markdown
+from telethon import types
+
+
 # -----------
+class CustomMarkdown:
+    @staticmethod
+    def parse(text):
+        text, entities = markdown.parse(text)
+        for i, e in enumerate(entities):
+            if isinstance(e, types.MessageEntityTextUrl):
+                if e.url == 'spoiler':
+                    entities[i] = types.MessageEntitySpoiler(e.offset, e.length)
+                elif e.url.startswith('emoji/'):
+                    entities[i] = types.MessageEntityCustomEmoji(e.offset, e.length, int(e.url.split('/')[1]))
+        return text, entities
+    @staticmethod
+    def unparse(text, entities):
+        for i, e in enumerate(entities or []):
+            if isinstance(e, types.MessageEntityCustomEmoji):
+                entities[i] = types.MessageEntityTextUrl(e.offset, e.length, f'emoji/{e.document_id}')
+            if isinstance(e, types.MessageEntitySpoiler):
+                entities[i] = types.MessageEntityTextUrl(e.offset, e.length, 'spoiler')
+        return markdown.unparse(text, entities)
 with open('config.json') as f:
     data = json.load(f)
     api_id = data['api_id']
