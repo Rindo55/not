1c37c512d871ebdd25f249754485a8dfcc0dd462
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-10-16 02:20:42 +0530

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -165,7 +165,7 @@ async def mai(event):
     client.parse_mode = CustomMarkdown()
     message += f'''\n\n<b>$TOM | The Token of 2024</b> <a href="emoji/5924664908158341416">🍅</a>'''
     
-    await client.send_message(entity=-2393055054, reply_to=msg.id, message=message, link_preview=True)
+    await client.send_message(entity=-1541249835, reply_to=msg.id, message=message, link_preview=True)
             # Only include price change in the
 # Run the bot
 with client:
